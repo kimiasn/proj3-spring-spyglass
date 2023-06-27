05345f0df3ed0752bc3e95288df4dacdecca0653
S Yee <yee@terpmail.umd.edu>
2023-06-27 19:55:53 -0400

    third half

--- src/test/java/com/skillstorm/demo/SpringSpyglassPlannerApplicationTests.java
+++ src/test/java/com/skillstorm/demo/SpringSpyglassPlannerApplicationTests.java
@@ -2,12 +2,14 @@ package com.skillstorm.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringSpyglassPlannerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(true);
 	}
 
 }
