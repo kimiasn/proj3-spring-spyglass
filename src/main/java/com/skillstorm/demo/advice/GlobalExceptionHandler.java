dafeacc02da6082923600e02c47c5567b46a9eec
kimiasn <62445592+kimiasn@users.noreply.github.com>
2023-08-12 10:48:18 -0400

    Update GlobalExceptionHandler.java

--- src/main/java/com/skillstorm/demo/advice/GlobalExceptionHandler.java
+++ src/main/java/com/skillstorm/demo/advice/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+* JAVA_DOC
+**/
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
